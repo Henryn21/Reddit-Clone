3693f07204ca74be50563f3bc8b63197f48af960
proffitlemon@gmail.com <45953538+Henryn21@users.noreply.github.com>
2023-03-03 18:49:30 -0800

    Finished layout and started js

--- src/index.js
+++ src/index.js
@@ -11,29 +11,58 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
+//test to read data on fb
 let showData=async ()=>{
     const docRef = doc(db, "yo", "NREu5f4BEGNfREtNXEDs");
     const docSnap = await getDoc(docRef);
     console.log(docSnap.data());
 }
 showData();
+//variables for post template, CHANGE TO CREATE NEW ELEMENTS!!!!
+// let titleElement=document.querySelector("#templateHeader");
+// let authorElement=document.querySelector("#templatePoster");
+// let communityElement=document.querySelector("#templateCommunity");
+// let contentElement=document.querySelector("#templateContent");
+//variables for post info
+let postJson;
+let title;
+let comm;
+let poster;
+let postContent;
 //fetch api on url with .json at end
-fetch("https://www.reddit.com/r/CK3AGOT/comments/116drje/which_of_these_late_features_are_you_the_most.json")
+fetch("https://www.reddit.com/r/cscareerquestions/comments/11gqslm/the_hustle_culture_posts_are_very_off_putting/.json")
     .then((res)=>{
         return res.json();
     })
-    .then((data)=>{
-        console.log(data);
+    .then((postInfo)=>{
+        // console.log(postInfo[0].data.children[0].data.);
+        //fix : ugly
+        let titleElement=document.querySelector("#templateTitle");
+        let authorElement=document.querySelector("#templatePoster");
+        let communityElement=document.querySelector("#templateCommunity");
+        let contentElement=document.querySelector("#templateContent");
+        //see above
+        postJson=postInfo[0].data.children[0].data;
+        poster=postJson.author;
+        title=postJson.title;
+        comm=postJson.subreddit;
+        //selftexthtml is a property, maybe creates elements? Not by default :(
+        postContent=postJson.selftext;
+        //write to html, FIX: swap to dynamic created elements or something
+        titleElement.textContent=title;
+        authorElement.textContent=poster;
+        communityElement.textContent=comm;
+        contentElement.textContent=postContent;
     })
     .catch(function(err) {
         console.log(err);   // Log error if any
     });
     //returns promise
-let getStuff= async ()=>{
-    let po= await fetch("https://www.reddit.com/r/CK3AGOT/comments/116drje/which_of_these_late_features_are_you_the_most.json");
-    let data=po.json();
+let getStuff= async (community, sort)=>{
+    let results= await fetch(`https://www.reddit.com/r/${community}/${sort}.json`);
+    let data=results.json();
     console.log(data);
+    return data;//build post components based on returned posts info
 }
     
 getStuff();
